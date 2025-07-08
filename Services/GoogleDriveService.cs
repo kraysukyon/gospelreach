caa620e668eaaf1201933fbd6f1beb383f5c5f96
kraysukyon <felipejosephthor@gmail.com>
2025-07-08 08:41:26 +0800

    modify login

--- Services/GoogleDriveService.cs
+++ Services/GoogleDriveService.cs
@@ -11,25 +11,6 @@ namespace GospelReachCapstone.Services
             _js = js;
         }
 
-        //public async Task<string> SignInAsync()
-        //{
-        //    try
-        //    {
-        //        var accessToken = await _js.InvokeAsync<string>(
-        //            "googleDriveFunctions.init",
-        //            "865916552971-6aj79obahgrlk7g9ts73hs788i2l1cgu.apps.googleusercontent.com",
-        //            "https://www.googleapis.com/auth/drive.readonly" // Use readonly unless you want upload/delete
-        //        );
-
-        //        return accessToken;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        await _js.InvokeVoidAsync("alert", $"Google Drive sign-in failed: {ex.Message}");
-        //        return null;
-        //    }
-        //}
-
         public async Task signInAsync()
         {
             try
---
+++ wwwroot/Javascript/firebase.js
@@ -0,0 +1,13 @@
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyD8al1qptb_WKQGuhRvPOFvTRAZPg-J4uY",
+    authDomain: "gospel-reach-cms.firebaseapp.com",
+    projectId: "gospel-reach-cms",
+    storageBucket: "gospel-reach-cms.firebasestorage.app",
+    messagingSenderId: "199203547088",
+    appId: "1:199203547088:web:dc1fc54cb156ddabf07bd5"
+};
+
+
+// ✅ Make accessible to JS interop
+window.firebase = firebase.initializeApp(fireFunction.getKey); //Initialize firebase
+window.db = firebase.firestore();
