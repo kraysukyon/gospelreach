6897a85bf1ad85a35fd2884dc7a8cb6533087be7
kraysukyon <felipejosephthor@gmail.com>
2025-09-17 14:02:54 +0800

    modify classes

---
+++ dist/wwwroot/Javascript/firebase-auth.js
@@ -0,0 +1,50 @@
+﻿window.firebaseAuth = {
+    register: async function (account) {
+        try {
+            const auth = firebase.auth();
+            const userCredential = await auth.createUserWithEmailAndPassword(account.email, account.password);
+            const uid = userCredential.user.uid;
+
+            const db = firebase.firestore();
+            //await db.collection("Accounts").add(user);
+            await db.collection("Users").doc(uid).set({
+                email: account.email,
+                firstName: account.firstName,
+                middleName: account.middleName,
+                lastName: account.lastName,
+                role: account.role,
+                status: account.status,
+            });
+
+            return { success: true, uid: userCredential.user.uid }
+        } catch (error) {
+            return {success: false, error: error.message}
+        }
+    },
+
+    resetPassword: async function (email) {
+        try {
+            const auth = firebase.auth();
+            await auth.sendPasswordResetEmail(email);
+            return { success: true };
+        } catch (error) {
+            return { success: false, error: error.message };
+        }
+    },
+    
+    login: async function (email,password) {
+        try {
+            const auth = firebase.auth();
+            const userCredential = await auth.signInWithEmailAndPassword(email, password);
+            const idToken = await userCredential.user.getIdToken(true);
+            return { success: true, uid: userCredential.user.uid, idToken: idToken }
+        } catch (error) {
+            return { success: false, error: error.message}
+        }
+    },
+
+    logout: async function () {
+        const auth = firebase.auth();
+        await auth.signOut();
+    }
+}
---
+++ dist/wwwroot/Javascript/firebase.js
@@ -0,0 +1,8 @@
+﻿window.initializeFirebase = function (config) {
+    window.firebase = firebase.initializeApp(config);
+    window.db = firebase.firestore();
+};
+
+ //Call this from Blazor with your Firebase config object
+ //Javascript/firebase.js
+
