6101ad825b0e8bc8a588e65eddb7b46da380ce66
kraysukyon <felipejosephthor@gmail.com>
2025-09-26 13:03:20 +0800

    modify firebase

---
+++ wwwroot/Javascript/firebase-notifications.js
@@ -0,0 +1,9 @@
+﻿export async function requestNotificationPermission() {
+    try {
+        const token = await messaging.getToken({ vapidKey: "BNlOKIO-yHAS3KByumFSIu1YRw3xyyAIbt5BcbpEX_4qXbb222P3qn8kHwX4BxhbR6KruylJwDhLhWlM3dP9qTU" });
+        return token; // Send to backend
+    } catch (err) {
+        console.error("Unable to get permission for notifications", err);
+        return null;
+    }
+}
--- wwwroot/Javascript/firebase.js
+++ wwwroot/Javascript/firebase.js
@@ -1,7 +1,22 @@
-﻿window.initializeFirebase = function (config) {
-    window.firebase = firebase.initializeApp(config);
-    window.db = firebase.firestore();
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyCuXJoMGNsbGubbWtKsBaCSt7OVwXrefgs",
+    authDomain: "gospel-reach-tsa-cms.firebaseapp.com",
+    projectId: "gospel-reach-tsa-cms",
+    storageBucket: "gospel-reach-tsa-cms.firebasestorage.app",
+    messagingSenderId: "810722788803",
+    appId: "1:810722788803:web:ecdfa0cd2b116aa6c22976",
+    measurementId: "G-CQ3ST5ZG3P"
 };
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const messaging = firebase.messaging();
+window.myFirebase = { app, db }; // expose to Blazor
+//window.initializeFirebase = function (config) {
+//    window.firebase = firebase.initializeApp(config);
+//    window.db = firebase.firestore();
+//};
 
  //Call this from Blazor with your Firebase config object
  //Javascript/firebase.js
